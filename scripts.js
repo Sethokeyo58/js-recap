c2c5a758f005e8c10d391b7f5aea6b05a0a7272c
Sethokeyo58 <sethokeyo58@gmail.com>
2025-07-21 11:03:08 +0300

    primitive data types

---
+++ scripts.js
@@ -0,0 +1,63 @@
+console.log('Hello,World');
+let firstName='John';
+console.log(firstName);
+let age = '20';
+
+//numbers
+let num = 10.56;
+console.log (typeof num);
+console.log (5 / 0);
+console.log (-5 / 0);
+
+console.log('Hi' / 2);
+console.log(Math.sqrt(-4));
+
+let largeNumber =5760746874707649999089n
+console.log(largeNumber);
+let lNumber =BigInt("64034560568996887798");
+console.log(lNumber);
+console.log(largeNumber + lNumber);
+console.log(largeNumber * lNumber);
+
+let num1 = 10;
+let num2 = 5;
+console.log (num1 + num2);
+console.log (num1 - num2);
+console.log (num1 / num2);
+console.log (num1 * num2);
+console.log (num1 % num2);
+console.log (num1 ** num2);  //exponensiation
+
+
+console.log(Math.sqrt(16));
+console.log(Math.floor(16.9));
+console.log(Math.ceil(16.1));
+console.log(Math.round(16.4547374));
+console.log(Math.round(16.256374).toFixed(2));
+
+
+//Area of Triangle
+let base = 10;
+let height = 17;
+
+console.log((0.5*(base))* height);
+
+let radius = 5;
+let height1 = 12;
+let volume = Math.PI* (radius**2) *height1;
+console.log(volume.toFixed(2));
+
+let isLoggedIn = true;
+let hasPermission = false;
+
+//undefined
+let lastName;
+console.log(lastName);
+console.log(typeof lastName);
+
+//null
+let gender =null;
+console.log(gender);
+console.log(typeof gender);
+
+
